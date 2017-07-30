33803bc4f387c6e148c3d9431875c7ed69de39fc
warmblack <cjw921218@163.com>
2017-07-30 22:06:31 +0800

    头部视图手势初步添加完成

--- CJWTakeAway/Classes/Main/CJWBaseViewController.h
+++ CJWTakeAway/Classes/Main/CJWBaseViewController.h
@@ -11,7 +11,7 @@
 @interface CJWBaseViewController : UIViewController
 
 //将导航条作为属性公开给子类,让子类可以自己定义属于自己的导航条样式
-@property (nonatomic ,strong) UINavigationBar *navBar;
+@property (nonatomic ,strong ,readonly) UINavigationBar *navBar;
 //navigationItem
-@property (nonatomic ,strong) UINavigationItem *navItem;
+@property (nonatomic ,strong ,readonly) UINavigationItem *navItem;
 @end
--- CJWTakeAway/Classes/Shop/Controller/CJWShopController.m
+++ CJWTakeAway/Classes/Shop/Controller/CJWShopController.m
@@ -11,29 +11,74 @@
 
 @interface CJWShopController ()
 
+//将头部视图定义成属性,在手势方法中更新约束
+@property (nonatomic ,weak) UIView *shopHeaderView;
 @end
 
 @implementation CJWShopController
 
 - (void)viewDidLoad {
-    [super viewDidLoad];
+   
     
+    //创建首页视图的头部视图
+    UIView *shopHeaderView = [[UIView alloc] init];
+    shopHeaderView.backgroundColor = [UIColor purpleColor];
+    //添加到父控制器
+    [self.view addSubview:shopHeaderView];
+    //约束
+    [shopHeaderView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.left.right.offset(0);
+        make.height.offset(180);
+        
+    }];
+    
+    //这个super执行的时候会去执行父类的代码,包括创建添加导航条
+     [super viewDidLoad];
     self.view.backgroundColor = [UIColor blueColor];
     
     self.navItem.title = @"我喜欢吃";
-}
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-
--(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
-{
-    CJWFoodDetailController *foodVC = [[CJWFoodDetailController alloc] init];
     
-    [self.navigationController pushViewController:foodVC animated:YES];
+    //创建手势
+    UIPanGestureRecognizer *pan = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(panGesture:)];
+    //添加手势
+    [self.view addGestureRecognizer:pan];
+    
+    
+    
+    _shopHeaderView = shopHeaderView;
+    
 }
+
+//手势方法
+- (void)panGesture:(UIPanGestureRecognizer *)pan
+{
+    //平移距离
+    CGPoint p = [pan translationInView:pan.view];
+    
+    //更新约束
+    [_shopHeaderView mas_updateConstraints:^(MASConstraintMaker *make) {
+       
+        make.height.offset(p.y + _shopHeaderView.bounds.size.height);
+        
+    }];
+    
+    //重置距离
+    [pan setTranslation:CGPointZero inView:pan.view];
+    
+}
+
+
+
+
+//-(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+//{
+//    CJWFoodDetailController *foodVC = [[CJWFoodDetailController alloc] init];
+//    
+//    [self.navigationController pushViewController:foodVC animated:YES];
+//}
+
+
+
 
 @end
