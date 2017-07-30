f23ff765d5852df2faacd753166fd2112c7a3c5c
warmblack <cjw921218@163.com>
2017-07-30 20:24:35 +0800

    Initial Commit

---
+++ CJWTakeAway/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CJWTakeAway
+//
+//  Created by admin on 2017/7/30.
+//  Copyright © 2017年 lym. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CJWTakeAway/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CJWTakeAway
+//
+//  Created by admin on 2017/7/30.
+//  Copyright © 2017年 lym. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
