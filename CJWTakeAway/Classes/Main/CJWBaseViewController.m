cd34dd3d6d971d6ec069f70a70d8b30c19ebb22e
warmblack <cjw921218@163.com>
2017-07-30 21:50:13 +0800

    为根控制器以外的所有子控制器添加导航条返回按钮 完成

--- CJWTakeAway/Classes/Main/CJWBaseViewController.m
+++ CJWTakeAway/Classes/Main/CJWBaseViewController.m
@@ -14,27 +14,41 @@
 
 @implementation CJWBaseViewController
 
+- (instancetype)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        
+        //添加导航栏
+        UINavigationBar *navBar = [[UINavigationBar alloc] init];
+       
+        
+        
+        UINavigationItem *navItem = [[UINavigationItem alloc] init];
+        //建立联系
+        [navBar setItems:@[navItem]];
+        
+        
+        
+        
+        _navBar = navBar;
+        _navItem = navItem;
+        
+        
+    }
+    return self;
+}
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    //添加导航栏
-    UINavigationBar *navBar = [[UINavigationBar alloc] init];
-    [self.view addSubview:navBar];
+  
+    [self.view addSubview:_navBar];
     //约束
-    [navBar mas_makeConstraints:^(MASConstraintMaker *make) {
+    [_navBar mas_makeConstraints:^(MASConstraintMaker *make) {
         make.top.left.right.offset(0);
         make.height.offset(64);
     }];
-    
-    
-    UINavigationItem *navItem = [[UINavigationItem alloc] init];
-    //建立联系
-    [navBar setItems:@[navItem]];
-    
-    
-    
-    
-    _navBar = navBar;
-    _navItem = navItem;
 }
 
 - (void)didReceiveMemoryWarning {
--- CJWTakeAway/Classes/Main/CJWNavigationController.m
+++ CJWTakeAway/Classes/Main/CJWNavigationController.m
@@ -7,6 +7,7 @@
 //
 
 #import "CJWNavigationController.h"
+#import "CJWBaseViewController.h"
 
 @interface CJWNavigationController ()
 
@@ -16,7 +17,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
+    //隐藏系统自带的导航栏,改用自定义的
     self.navigationBar.hidden = YES;
     
     
@@ -26,5 +27,27 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
+
+-(void)pushViewController:(CJWBaseViewController *)viewController animated:(BOOL)animated{
+    [super pushViewController:viewController animated:animated];
+    //当不是根控制器的时候,在导航栏上创建一个返回按钮
+    if (self.childViewControllers.count > 1) {
+        UIBarButtonItem *leftBarBtn = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"btn_backItem"] style:UIBarButtonItemStylePlain target:self action:@selector(backToLastVC)];
+        viewController.navItem.leftBarButtonItem = leftBarBtn;
+        
+        //刚开始设置完不显示的原因是因为刚开始是在基类中的viewdidload里面添加的导航栏,但是当上面这些代码执行的时候,基类中的viewdidload还没有执行,所以其中的navbar和navitem都没有值,所以在这边不能修改.所以将基类中创建navbar等代码执行优先级提前,放在初始化控制器的时候执行.
+}
+    
+}
+
+//返回上一个控制器
+-(void)backToLastVC
+{
+    [self popViewControllerAnimated:YES];
+}
+
+
 
 @end
